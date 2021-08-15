39ad8fece555ebdfae6cd9bdbb7ffc44ed787853
Lev Nagornov <lnagornov@yahoo.com>
2021-08-15 17:29:41 +0300

    fix first filling db script

--- fill_db_with_data/fill_db_with_data.py
+++ fill_db_with_data/fill_db_with_data.py
@@ -1,35 +1,60 @@
+"""Use this file to first fill database with prepared data from csv files."""
+
+import sys
+import os
 import csv
+
+
+#to import module "models.py" from parent dir
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+if parent not in sys.path:
+    sys.path.append(parent)
+
+
 from models import *
+from app import *
 
 
-"""
-id, title = categories
-id, title, price, description, picture, category_id = meals
-"""
+with app.app_context():
+    categories = db.session.query(Category).all()
+    if categories is None:
+        categories_data_path = os.path.join(current, "categories.csv")
+        with open(categories_data_path, encoding="UTF8") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
 
+            next(csv_reader) #skip first row with header
 
-with open('categories.csv', encoding="UTF8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',', )
-    next(csv_reader) #skip first row with header
-    for row in csv_reader:
-        # print(row[0], row[1])
-        category = Category(
-            title = row[1]
-        )
-        db.session.add(category)
-    db.session.commit()    
+            for row in csv_reader:
+                print(row[1])
+                new_category = Category(title=row[1])
+                db.session.add(new_category)
+            db.session.commit()    
 
+            print(">>>Categories -----> done")
+    else:
+        print("Category table is not empty")
 
-with open('meals.csv', encoding="UTF8") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',', )
-    next(csv_reader) #skip first row with header
-    for row in csv_reader:
-        print(row[0], row[1], row[2], row[3], row[4], row[5])
-        dish = Dish(
-            title = row[1],
-            price = row[1],
-            description = row[1],
-            picture = row[1],
-        )
-        db.session.add(dish)
-    db.session.commit()   
+    categories = db.session.query(Category).all()
+    if categories is None:
+        dishes_data_path = os.path.join(current, "meals.csv")
+        with open(dishes_data_path, encoding="UTF8") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',', )
+
+            next(csv_reader) #skip first row with header
+
+            for row in csv_reader:
+
+                dish = Dish(
+                    title = row[1],
+                    price = row[2],
+                    description = row[3],
+                    picture = row[4],
+                    category_id = row[5]
+                )
+                db.session.add(dish)
+            db.session.commit()   
+
+            print(">>>Dishes -----> done")
+    else:
+        print("Dish table is not empty")
