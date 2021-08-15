29297d4fcea3a29d406afd39114a8f3dac9b5ee7
Lev Nagornov <lnagornov@yahoo.com>
2021-08-15 17:28:21 +0300

    add returns

--- views/views.py
+++ views/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, request, Blueprint
 
+from models import db
 
 view_blp = Blueprint("view_blp", __name__)
 
@@ -7,40 +8,47 @@ view_blp = Blueprint("view_blp", __name__)
 @view_blp.route("/", methods=["GET", "POST"])
 def render_index():
     """Main page"""
-    pass
+
+    return render_template("index.html")
 
 
 @view_blp.route("/cart/", methods=["GET", "POST"])
 def render_cart():
     """Cart page with user's selected products"""
-    pass
+    
+    return render_template("cart.html")
 
 
 @view_blp.route("/account/", methods=["GET", "POST"])
 def render_account():
     """Personal account page"""
-    pass
+
+    return render_template("account.html")
 
 
 @view_blp.route("/auth/", methods=["GET", "POST"])
 def render_auth():
     """Authentication page"""
-    pass
+
+    return render_template("auth.html")
 
 
 @view_blp.route("/register/", methods=["GET", "POST"])
 def render_register():
     """User register page"""
-    pass
+    
+    return render_template("register.html")
 
 
 @view_blp.route("/logout/", methods=["GET", "POST"])
 def render_logout():
     """Logout of authorized user page"""
-    pass
+
+    return render_template("logout.html")
 
 
 @view_blp.route("/ordered/", methods=["GET", "POST"])
 def render_ordered():
     "Submit of order page"
-    pass
+
+    return render_template("ordered.html")
