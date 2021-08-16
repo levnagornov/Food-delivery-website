68efe4b473ec11c9e3a327d8ff9648fd7ba487a9
Lev Nagornov <lnagornov@yahoo.com>
2021-08-16 23:22:28 +0300

    add cart page

--- views/views.py
+++ views/views.py
@@ -1,6 +1,12 @@
-from flask import render_template, request, Blueprint
+from collections import Counter, OrderedDict
 
-from models import db
+from flask import render_template, request, Blueprint, session, redirect
+from flask.helpers import url_for
+
+from sqlalchemy.sql import func
+
+from models import *
+
 
 view_blp = Blueprint("view_blp", __name__)
 
@@ -9,14 +15,66 @@ view_blp = Blueprint("view_blp", __name__)
 def render_index():
     """Main page"""
 
-    return render_template("index.html")
+    categories = db.session.query(Category).all()
+    category_with_dishes = {
+        category.title : db.session.query(Dish) \
+                            .filter(Dish.category_id==category.id) \
+                            .order_by(func.random()) \
+                            .limit(3)
+        for category in categories
+    }
+
+    return render_template(
+        "index.html", 
+        category_with_dishes=category_with_dishes
+    )
+
+@view_blp.route("/addtocart/<dish_id>", methods=["GET", "POST"])
+def render_addtocart(dish_id):
+    dish = db.session.query(Dish).get(dish_id)
+    if dish is None:
+        return render_page_not_found(404, msg="Такого товара не существует.")
+    
+    cart = session.get("cart", OrderedDict())
+    if not cart.get(dish_id):
+        cart[dish_id] = 1
+    else:
+        cart[dish_id] += 1
+    session['cart'] = cart
+
+    return redirect(url_for("view_blp.render_cart"))
+
+
+@view_blp.route("/resetcart/", methods=["GET", "POST"])
+def render_resetcart():
+    """Endpoint to reset user cart"""
+
+    session.pop('cart')
+
+    return redirect(url_for("view_blp.render_cart"))
 
 
 @view_blp.route("/cart/", methods=["GET", "POST"])
 def render_cart():
     """Cart page with user's selected products"""
-    
-    return render_template("cart.html")
+
+    cart = session.get("cart", {})
+    cart_dish_amount = [
+        (db.session.query(Dish).get(int(dish_id)), amount)
+        for dish_id, amount in cart.items()
+    ]
+
+    session['order_amount'] = len(cart_dish_amount)
+    session['order_sum'] = sum(
+        [dish.price * amount for dish, amount in cart_dish_amount]
+    )
+
+    return render_template(
+        "cart.html", 
+        cart=cart_dish_amount, 
+        order_sum=session['order_sum'],
+        order_amount=session['order_amount']
+    )
 
 
 @view_blp.route("/account/", methods=["GET", "POST"])
@@ -52,3 +110,28 @@ def render_ordered():
     "Submit of order page"
 
     return render_template("ordered.html")
+
+
+@view_blp.errorhandler(500)
+def render_server_error(error, msg="Что-то не так, но мы все починим!"):
+    """Handling 500 error."""
+
+    return render_template("error.html", msg=msg), 500
+
+
+@view_blp.errorhandler(404)
+def render_page_not_found(
+    error, msg="Ничего не нашлось! Вот неудача, отправляйтесь на главную!"
+):
+    """Handling 404 error"""
+
+    return render_template("error.html", msg=msg), 404
+
+
+@view_blp.errorhandler(400)
+def render_bad_request(
+    error, msg="Ошибка запроса! Вот неудача, отправляйтесь на главную или измените запрос!"
+):
+    """Handling 400 error"""
+
+    return render_template("error.html", msg=msg), 400
